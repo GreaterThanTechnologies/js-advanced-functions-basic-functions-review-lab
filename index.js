ef95a794ce3c9a13fbb14903c40fbe9415f81049
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-04-26 14:50:39 -0400

    commit tree f3ae7849df845a71c8092cebc7bd3d49eef8aabd

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(par="*") {
+  return function(adjective="special") {
+    return `You are ${par}${adjective}${par}!`
+  }
+}
+
+let Calculator = {
+  add: function(a,b) {
+    return a + b
+  },
+  subtract: function(a,b) {
+    return a - b
+  },
+  multiply: function(a,b) {
+    return a * b
+  },
+  divide: function(a,b) {
+    return a/b
+  }
+}
+
+let actionApplyer = function(start, ray) {
+  let s = start
+  for (let i = 0; i < ray.length; i++) {
+    s = ray[i](s)
+  }
+  return s
+}
